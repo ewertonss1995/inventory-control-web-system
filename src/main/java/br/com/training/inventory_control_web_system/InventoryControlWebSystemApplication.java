ab4385f8f0aceae0587b8115a19ee3dc02cba17a
Ewerton Silva <ewertonsilva.fro@gmail.com>
2026-03-11 11:04:49 -0300

    Projeto criado

---
+++ src/main/java/br/com/training/inventory_control_web_system/InventoryControlWebSystemApplication.java
@@ -0,0 +1,13 @@
+package br.com.training.inventory_control_web_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventoryControlWebSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventoryControlWebSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/training/inventory_control_web_system/InventoryControlWebSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.training.inventory_control_web_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryControlWebSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
