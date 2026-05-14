4f9e4854b1fc58709bfde2ae297af7db9e31b454
ewertonss1995 <ewertonsilva.fro@gmail.com>
2026-05-14 15:09:20 -0300

    test 8

--- infra/variables.tf
+++ infra/variables.tf
@@ -7,7 +7,7 @@ variable "resource_group_name" {
 variable "location" {
   description = "Região do Azure"
   type        = string
-  default     = "uksouth"
+  default     = "eastus2"
 }
 
 variable "admin_password" {
