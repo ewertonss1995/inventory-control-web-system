583b0713329814e420266b39dad18dd136f89913
ewertonss1995 <ewertonsilva.fro@gmail.com>
2026-05-15 08:47:33 -0300

    Fix storage account name

--- infra/backend.tf
+++ infra/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "rg-tfstate"
-    storage_account_name = "tfstateivtsys4504"
+    storage_account_name = "tfstateivtsys7503"
     container_name       = "tfstate"
     key                  = "terraform.tfstate"
   }
